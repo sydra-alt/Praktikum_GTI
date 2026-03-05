775992f2742bd5825785e072a4ae2e0e37a5e35b
Syahrafi Ahmad Pradika <Syahrafipradika@gmail.com>
2026-03-05 09:25:26 +0700

    Initial Commit

---
+++ Praktikum1_GT/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Praktikum1_GT
+//
+//  Created by Syahrafi Ahmad Pradika on 05/03/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
